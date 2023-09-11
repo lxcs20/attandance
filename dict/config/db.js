979c7595b93c26d95c2728193907c776b7e62920
Lanthong <98867063+lxcs20@users.noreply.github.com>
2023-09-11 12:25:48 +0700

    .

--- dict/config/db.js
+++ dict/config/db.js
@@ -7,14 +7,19 @@ exports.AttandanceEntity = exports.UserEntity = void 0;
 const user_model_1 = __importDefault(require("../model/user.model"));
 const attandance_model_1 = __importDefault(require("../model/attandance.model"));
 const key_1 = require("./key");
-const sequelize_1 = __importDefault(require("sequelize"));
-// const db_Connection: Sequelize = new Sequelize(DB_URI)
-const db_Connection = new sequelize_1.default
-    .Sequelize(key_1.DB_NAME, key_1.DB_USERNAME, key_1.DB_PASSWORD, {
-    port: key_1.DB_PORT,
-    host: key_1.DB_HOST,
-    dialect: 'postgres'
-});
+const sequelize_1 = require("sequelize");
+const db_Connection = new sequelize_1.Sequelize(key_1.DB_URI);
+// const db_Connection: Sequelize = new sequelize
+// .Sequelize(
+//     DB_NAME,
+//     DB_USERNAME,
+//     DB_PASSWORD,
+//     {
+//         port: DB_PORT,
+//         host: DB_HOST,
+//         dialect: 'postgres'
+//     }
+// )
 db_Connection.sync({ alter: true });
 exports.UserEntity = (0, user_model_1.default)('user', db_Connection);
 // export let LeaveEntity = LeaveFactory('leave', db_Connection);
